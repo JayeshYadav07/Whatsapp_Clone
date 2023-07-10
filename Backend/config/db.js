11b85e86aab41523c28468a8bead5b7ffec09f0a
jayeshyadav07 <jayeshyadav8080@gmail.com>
2023-07-10 13:21:48 -0700

    fixed syntax

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connection = mongoose.connect(process.env.MONGODB_URL);
 module.exports = {
-    connection,
+    connection
 };
